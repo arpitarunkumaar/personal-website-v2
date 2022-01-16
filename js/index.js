1b49c56382e8b18f71944c1038aa07e65aa23d57
Arpit Arun Kumaar <arpitak14@outlook.com>
2022-01-16 01:37:43 -0500

    made several ui changes

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
+import Darkmode from "darkmode-js";
+
 // MENU SHOW
 const showMenu = (toggleId, navId) => {
   const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId);
 
-  if (toggle && nav) 
-  {
+  if (toggle && nav) {
     toggle.addEventListener("click", () => {
       nav.classList.toggle("show");
     });
@@ -15,70 +16,89 @@ showMenu("nav-toggle", "nav-menu");
 // REMOVE MENU MOBILE
 const navLink = document.querySelectorAll(".nav__link");
 
-function linkAction() 
-{
+function linkAction() {
   const navMenu = document.getElementById("nav-menu");
   navMenu.classList.remove("show");
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 // SCROLL SECTIONS
-const sections = document.querySelectorAll('section[id]')
+const sections = document.querySelectorAll("section[id]");
 
-window.addEventListener('scroll', scrollActive)
+window.addEventListener("scroll", scrollActive);
 
-function scrollActive()
-{
-    const scrollY = window.pageYOffset
+function scrollActive() {
+  const scrollY = window.pageYOffset;
 
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight)
-        {
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
-        } else
-        {
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
-        }
-    })
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active");
+    }
+  });
 }
 
 // SCROLL ANIMATION
-const sr = ScrollReveal
-({
-    origin: 'top',
-    distance: '80px',
-    duration: 2000,
-    reset: true
-})
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
 
 // SCROLL TO PAGE:
 
 /*SCROLL HOME*/
-sr.reveal('.home__title', {})
-sr.reveal('.home__scroll', {delay: 200})
-sr.reveal('.home__img', {origin:'right', delay: 400})
+sr.reveal(".home__title", {});
+sr.reveal(".home__scroll", { delay: 200 });
+sr.reveal(".home__img", { origin: "right", delay: 400 });
 
 /*SCROLL ABOUT*/
-sr.reveal('.about__img', {delay: 500})
-sr.reveal('.about__subtitle', {delay: 300})
-sr.reveal('.about__profession', {delay: 400})
-sr.reveal('.about__text', {delay: 500})
-sr.reveal('.about__social-icon', {delay: 600, interval: 200})
+sr.reveal(".about__img", { delay: 500 });
+sr.reveal(".about__subtitle", { delay: 300 });
+sr.reveal(".about__profession", { delay: 400 });
+sr.reveal(".about__text", { delay: 500 });
+sr.reveal(".about__social-icon", { delay: 600, interval: 200 });
 
 /*SCROLL SKILLS*/
-sr.reveal('.skills__subtitle', {})
-sr.reveal('.skills__name', {distance: '20px', delay: 50, interval: 100})
-sr.reveal('.skills__img', {delay: 400})
+sr.reveal(".skills__subtitle", {});
+sr.reveal(".skills__name", { distance: "20px", delay: 50, interval: 100 });
+sr.reveal(".skills__img", { delay: 400 });
 
 /*SCROLL PORTFOLIO*/
-sr.reveal('.portfolio__img', {interval: 200})
+sr.reveal(".portfolio__img", { interval: 200 });
 
 /*SCROLL CONTACT*/
-sr.reveal('.contact__subtitle', {})
-sr.reveal('.contact__text', {interval: 200})
-sr.reveal('.contact__input', {delay: 400})
-sr.reveal('.contact__button', {delay: 600})
+sr.reveal(".contact__subtitle", {});
+sr.reveal(".contact__text", { interval: 200 });
+sr.reveal(".contact__input", { delay: 400 });
+sr.reveal(".contact__button", { delay: 600 });
+
+// DARK MODE TOGGLE
+const options = 
+{
+  bottom: "6400px", // default: '32px'
+  right: "unset", // default: '32px'
+  left: "32px", // default: 'unset'
+  time: "0.5s", // default: '0.3s'
+  mixColor: "#fff", // default: '#fff'
+  backgroundColor: "#ffc0cb", // default: '#fff'
+  buttonColorDark: "#ffc0cb", // default: '#100f2c'
+  buttonColorLight: "#fff", // default: '#fff'
+  saveInCookies: false, // default: true,
+  label: "🌓", // default: ''
+  autoMatchOsTheme: true, // default: true
+};
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
+
